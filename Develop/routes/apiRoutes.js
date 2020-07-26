468f4c5d22d7350b37132c1a43d585c0b19e0cbe
Michael Li <dev.mike011235@gmail.com>
2020-07-25 23:23:13 -0400

    Fixed bug where two notes may have the same ID adding logci to the DELETE request that reassigns IDs of everything that is to the right of what was deleted.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,9 +1,9 @@
-const path = require("path")
 const apiRouter = require("express").Router()
 const db = require("../db/db.json") //temporary memory 
 const fs = require("fs")
 
-let id = db.length+1
+// Keeps track of what ID number is not assigned. 
+let id = db.length + 1
 
 // Return all saved notes in the db file.
 apiRouter.get("/notes", (req, res) => {
@@ -12,28 +12,37 @@ apiRouter.get("/notes", (req, res) => {
 
 // Save one note to the db file.
 apiRouter.post("/notes", (req, res) => {
+    // Give the body an ID key.
     req.body.id = id++
     db.push(req.body)
     fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
         if (err) throw err
-        else {res.json(db)}
+        else { res.json(db) }
     })
 })
 
-//
+// Delete notes with the corresponding ID.
 apiRouter.delete("/notes/:id", (req, res) => {
+    let deletedIndex = 0
+    let deletedID = 0
     console.log(req.params.id);
     for (let i = 0; i < db.length; i++) {
-         if (db[i].id === parseInt(req.params.id)) {
-             db.splice(i, 1)
-         }
-     }
-     fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
+        if (db[i].id === parseInt(req.params.id)) {
+            correctID_startPoint = db[i]
+            deletedID = db[i].id
+            db.splice(i, 1)
+        }
+    }
+    // When db[i] is deleted, ith + 1  through nth index will have it's id key updated.
+    // This solves the issue of it being possible for multiple IDs to be given to different notes. 
+    for (let j = deletedIndex; j < db.length; j++) {
+        db[j].id = deletedID
+        deletedID++
+    }
+    fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
         if (err) throw err
-        else {res.json(db)}
+        else { res.json(db) }
     })
 })
-
-
 
 module.exports = apiRouter
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,15 +1,14 @@
 const path = require("path")
 const htmlRouter = require("express").Router()
 
-
-
+// Redirects user to main page.
 htmlRouter.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
+// Redirects user to notes taking page. 
 htmlRouter.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
-
 
 module.exports = htmlRouter
